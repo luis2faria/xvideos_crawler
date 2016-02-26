30cddecb8aa6d7033823a054f581dc9c1d5f97cb
luis2faria <pvtluis@hotmail.com>
2016-02-26 20:45:25 +0000

    Update xvideos.php

--- xvideos.php
+++ xvideos.php
@@ -1,5 +1,8 @@
 <?php
 
+$list = fopen('list.txt','a+');
+$logs = fopen('logs.txt','a+');
+
 $var = file_get_contents( 'http://www.xvideos.com/tags' );
 $var = explode( '<ul id="tags">' , $var );
 $var = explode( '<div class="main-categories">' , $var[1] );
@@ -18,15 +21,15 @@ $main = true;
 while(true){
 	
 	if ( $main === false ) { $i = $i + 1; $main = true; }
-	if ( $i === count($links) ){ exit; }
+	if ( $i === count($links) ){ echo '<br /><br /><center><h1>.:: Concluido ::.</h1></center>'; exit; }
 	
 	while($main === true ){
 		$a = $a + 1;
 		
+		fwrite( $logs , $links[$i] . '/' . $a. "\n" );
 		#echo $links[$i] . '/' . $a;
 		
 		$links_var = file_get_contents($links[$i] . '/' . $a);
-		#flush();
 		
 		$links_var = explode( '<div class="mozaique">' , $links_var );
 		$links_var = explode( '<div class="pagination ">' , $links_var[1] );
@@ -35,10 +38,12 @@ while(true){
 		preg_match_all( '/<a href=\"([^\"]*)\">(.*)<\/a>/iU' , $links_var , $match );
 	
 		$links_main_var = $match[1];
-		if( empty( $links_main_var ) ){ $main = false; $a = 0; } else { $main = true; }
+		
+		if( count($links_main_var) <= 19 ){ $main = false; $a = 0; } else { $main = true; }
 	
 		foreach( $links_main_var as $tmp ){
-			$links_main[] = 'http://www.xvideos.com' . $tmp; 
+			fwrite( $list , 'http://www.xvideos.com' . $tmp . "\n");
+			#$links_main[] = 'http://www.xvideos.com' . $tmp; 
 			#echo $tmp . '<br />';
 			#flush();
 		}
