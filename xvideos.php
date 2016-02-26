d072c3dcc98caec29e4b86bf0656c4cd9ad0cd50
luis2faria <pvtluis@hotmail.com>
2016-02-26 17:39:29 +0000

    Update xvideos.php

--- xvideos.php
+++ xvideos.php
@@ -1,26 +1,31 @@
 <?php
-
 $var = file_get_contents( 'http://www.xvideos.com/tags' );
 $var = explode( '<ul id="tags">' , $var );
 $var = explode( '<div class="main-categories">' , $var[1] );
 $var = $var[0];
 preg_match_all( '/<a href=\"([^\"]*)\">(.*)<\/a>/iU' , $var , $match );
 $links_match = $match[1];
-
 foreach($links_match as $tmp){
 	$links[] = 'http://www.xvideos.com' . $tmp;
 }
-
 # $links (TAGS DO XVIDEOS)
 
 $a = 0;
+$i = 0;
 $main = true;
 
-for( $i = 0; $i<count($links); $i++ ){
+while(true){
 	
-	while($main === true){
+	if ( $main === false ) { $i = $i + 1; $main = true; }
+	if ( $i === (count($links)-1) ){ exit; }
+	
+	while($main === true ){
 		$a = $a + 1;
+		echo $links[$i] . '/' . $a;
+		
 		$links_var = file_get_contents($links[$i] . '/' . $a);
+		flush();
+		
 		$links_var = explode( '<div class="mozaique">' , $links_var );
 		$links_var = explode( '<div class="pagination ">' , $links_var[1] );
 		$links_var = $links_var[0];
@@ -37,6 +42,7 @@ for( $i = 0; $i<count($links); $i++ ){
 		}
 	}
 	# $links_main (LINKS DO SITE)
+	#var_dump($links_main);
+	
 }
-
 ?>
