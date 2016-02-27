8f603268fc82b2676bf640a7a459184069b081c6
luis2faria <pvtluis@hotmail.com>
2016-02-27 16:17:35 +0000

    Create apaga_duplicados.php

---
+++ apaga_duplicados.php
@@ -0,0 +1,15 @@
+<?php
+
+$fp = fopen( 'xlist.txt' , 'a+' );
+
+$var = file_get_contents( 'list.txt' );
+$var = explode( "\n" , $var );
+$var = array_values ( array_unique ( $var ) );
+
+foreach( $var as $tmp ){
+	fwrite( $fp, $tmp . "\n" );
+}
+
+fclose($fp);
+
+?>
